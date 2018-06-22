94073466b92777cf938ceeff0827cd6e13aebe2d
abj240 <fabian.schimke@haw-hamburg.de>
2018-06-22 22:53:28 +0200

    make pre_processor_images_controller stateless and threadsafe

--- app/controllers/pre_process_images_controller.rb
+++ app/controllers/pre_process_images_controller.rb
@@ -5,43 +5,38 @@ class PreProcessImagesController < ApplicationController
   def create
     begin_preprocessing = Time.now
 
-    data_url = params['base64'];
-    data_url = remove_mime_type(data_url);
+    image_url     = params['base64'];
+    image_old_url = params['base64_old'];
+    image_url     = remove_mime_type(image_url);
+    image_old_url = remove_mime_type(image_old_url);
+    image     = MiniMagick::Image.read(Base64.decode64(image_url))
+    image_old = MiniMagick::Image.read(Base64.decode64(image_old_url))
+    image.format     'png'
+    image_old.format 'png'
 
-    image = MiniMagick::Image.read(Base64.decode64(data_url))
-    image.format 'png'
-    # image.write "app/image.png"
+    thread_id = Thread.current.object_id
 
-    # data_url2 = params['base64_old'];
-    # data_url2 = remove_mime_type(data_url2);
 
-    # image2 = MiniMagick::Image.read(Base64.decode64(data_url2))
-    # image2.format 'png'
-    # image2.write "app/image_old.png"
-
-    #TODO use old image of payload to become stateless
-    image_old = MiniMagick::Image.open("app/image-temp.png")
-    image.write "app/image-temp.png"
-
-    #TODO clean up, write to unique tempfile
+    #TODO clean up
+    compared_path = "tmp/images/compared-#{thread_id.to_s}.png"
     compare = MiniMagick::Tool::Compare.new
-    compare << "-compose" << "src" << "-highlight-color" << "#AAAAAA" << "-lowlight-color" << "White" << "-background" << "White" << "-transparent-color"<< "White" << image.path << image_old.path << "app/compared.png"
+    compare << "-compose" << "src" << "-highlight-color" << "#AAAAAA" << "-lowlight-color" << "White" << "-background" << "White" << "-transparent-color"<< "White" << image.path << image_old.path << compared_path
     begin
       compare.call
     rescue
     end
 
-    transparent = MiniMagick::Image.open("app/compared.png")
-    transparent.transparent "#AAAAAA"
 
-    #TODO write to unique tempfile
+    transparent_highlighted = MiniMagick::Image.open(compared_path)
+    transparent_highlighted.transparent "#AAAAAA"
+
+    composited_path = "tmp/images/composited-#{thread_id}.png"
     composite = MiniMagick::Tool::Composite.new
-    composite << "-alpha" << "on" << transparent.path << image.path << "app/result.png"
+    composite << "-alpha" << "on" << transparent_highlighted.path << image.path << composited_path
     composite.call
 
-    transparent2 = MiniMagick::Image.open("app/result.png")
-    transparent2.transparent "White"
-    # transparent2.write "app/transparent.png"
+    transparent_lowlighted = MiniMagick::Image.open(composited_path)
+    transparent_lowlighted.transparent "White"
 
     puts "preprocessing time: " + (Time.now - begin_preprocessing).to_s
 
@@ -49,7 +44,7 @@ class PreProcessImagesController < ApplicationController
     begin_postprocessing = Time.now
 
     #TODO extract request to private method
-    file_content = open(transparent2.path) { |f| f.read }
+    file_content = open(transparent_lowlighted.path) { |f| f.read }
     payload = {"base64": Base64.strict_encode64(file_content)}
 
     #TODO needs to be set during startup
